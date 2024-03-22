0e965d474a89816d6ad418185c48a151837f745b
Jackson0077 <158521696+Jackson0077@users.noreply.github.com>
2024-03-22 18:13:16 -0500

    Update Game.java
    deleted unnecessary imports and made in no longer extend WebSocketServer, also deleted the overrides for WebSocketServer, and added startGame() function

--- src/main/java/uta/cse3310/Game.java
+++ src/main/java/uta/cse3310/Game.java
@@ -1,50 +1,11 @@
 package uta.cse3310;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.util.Collections;
-
-import org.java_websocket.WebSocket;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.Vector;
-import java.util.ArrayList;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-
-public class Game extends WebSocketServer {
+public class Game {
 
     private char[][] grid = new char[50][50];
     private ArrayList<String> words = new ArrayList<String>();
-  
-    @Override
-    public void onOpen(WebSocket conn, ClientHandshake handshake) {
-    }
 
-    @Override
-    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, String message) {
-    }
-
-    @Override
-    public void onError(WebSocket conn, Exception ex) {
-    }
-
-    @Override
-    public void onStart() {
+    public void startGame(){
     }
 
     public void initializeGrid(){
