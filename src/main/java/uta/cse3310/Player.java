b997c86b4dd4a889d878655ee3c6651b26cc822d
Echo82 <epublic@grandin.org>
2024-03-22 17:56:18 -0500

    Updated Player.java to include getters and setters

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -8,3 +8,34 @@ public class Player {
     boolean readyStatus;
 }
 
+public int getScore() {
+  return 0;
+} 
+public void setScore(int score) {
+
+}
+public int getWins() {
+  return 0;
+}
+public void setWins(int wins) {
+
+}
+public String getName() {
+  return "";
+} 
+public void setName(String name) {
+
+}
+public String getColor() {
+  return "";
+} 
+public void setColor(String color) {
+
+}
+public boolean getReady() {
+  return true;
+} 
+public void setReady(String name) {
+
+}
+
