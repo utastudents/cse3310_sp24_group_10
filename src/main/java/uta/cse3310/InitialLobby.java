3f8b78fb3313ace5a285ea053cd1ea2d57424fe9
Jackson0077 <158521696+Jackson0077@users.noreply.github.com>
2024-03-22 18:06:16 -0500

    Update InitialLobby.java
    Made the initial lobby extend WebSocketServer and added the necessary overrides

--- src/main/java/uta/cse3310/InitialLobby.java
+++ src/main/java/uta/cse3310/InitialLobby.java
@@ -1,11 +1,54 @@
 package uta.cse3310;
 
-public class InitialLobby {
+package uta.cse3310;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class InitialLobby extends WebSocketServer{
     public static int totalGames; //increments and decrements
     private int gameID;
     public Player player;
     //public int playerSize; //dependent on button clicked
-   
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+    }
+
+    @Override
+    public void onStart() {
+    }
+    
     public int getGameID(){
         /* Getter for gameID; Necessary for Game Lobby*/
         return this.gameID;
