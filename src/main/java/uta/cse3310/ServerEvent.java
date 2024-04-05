6ca2d655b5b41513b5b875f9ab6a93f9a22cfd2b
Jackson0077 <158521696+Jackson0077@users.noreply.github.com>
2024-04-04 20:34:59 -0500

    Update ServerEvent.java

--- src/main/java/uta/cse3310/ServerEvent.java
+++ src/main/java/uta/cse3310/ServerEvent.java
@@ -1,8 +1,6 @@
 package uta.cse3310;
 
 public class ServerEvent {
-    PlayerType YouAre; // Either an PLAYER1, PLAYER2, PLAYER3, PLAYER4
-    int GameId;
     int NumOfPlayers;
-    int PlayerId;
+    int PlayerIdx;
 }
