fb9695e6329ef19f247fa2d90904a0d11edff6d6
Jackson0077 <158521696+Jackson0077@users.noreply.github.com>
2024-04-04 20:34:21 -0500

    Update UserEvent.java

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -2,8 +2,6 @@ package uta.cse3310;
 // User events are sent from the webpage to the server
 
 public class UserEvent {
-    int PlayerId;
-    PlayerType PlayerIdx; // either an XPLAYER or an OPLAYER
-    // int Button; // button number from 0 to 8
+    int PlayerIdx;
     String PlayerName; //name inputed by the player
 }
