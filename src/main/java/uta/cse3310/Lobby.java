54c965b26458eb4d618f66510387ee9071960c45
alisaawilliams <117318757+alisaawilliams@users.noreply.github.com>
2024-04-07 16:39:31 -0500

    Update Lobby.java

--- src/main/java/uta/cse3310/Lobby.java
+++ src/main/java/uta/cse3310/Lobby.java
@@ -2,9 +2,25 @@ package uta.cse3310;
 
 public class Lobby {
     private Player ready;
-    
-    public void isReady(Player ready){
+    private static boolean player1ready = false;
+    private static boolean player2ready = false;
+
+    public void isReady(int playerID){
         /* Allow player to toggle their respective ready status */
+        if(playerID == 1)
+        {
+            player1ready = !player1ready;
+        }
+        if(playerID==2)
+        {
+            player2ready = !player2ready;
+        }
+    }
+    public boolean readyCheck(){
+        retuen player1ready && player2ready; 
+    }    
+
+        
     }
     public int leave(){
         int leave = 0;
@@ -17,3 +33,4 @@ public class Lobby {
     }
 */
 }
+
