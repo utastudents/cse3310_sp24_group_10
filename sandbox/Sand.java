9a70bf7c03c03c30eff1a6e6abaeadfc36b3dab9
Echo82 <epublic@grandin.org>
2024-04-11 12:20:09 -0500

    Added random number generator with seed

--- sandbox/Sand.java
+++ sandbox/Sand.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Sand {
   public static void main(String[] args) {
     printGrid(createGrid());
@@ -5,34 +8,36 @@ public class Sand {
 
   public static char[][] createGrid()
   {
-    int size = 50;
+    final int size = 100;
+    
+    Random rand_num = new Random();
     //char[][] my_array = { {'a','a','c'},
                           //{'b','b','c','b'} };
-    char[][] my_array = {};
+    char[][] my_array = new char[size][size];
 
     for ( int x = 0; x < size; x ++)
     {
       for ( int y = 0; y < size; y ++)
       {
-        my_array[x][y] = 'a';
+        // 'z' + 1 because upper bound is exclusive
+        my_array[x][y] = (char) (rand_num.nextInt('z' + 1 - 'a') + 'a');
       }
     }
-
 
     return my_array;
   }
 
   public static void printGrid(char[][] my_array)
   {
-    System.out.format("Length of array is: %d\n", my_array[1].length);
+    //System.out.format("Length of array is: %d\n", my_array[0].length);
+    //System.out.format("Length of array is: %d\n", my_array[1].length);
     for ( int i = 0; i < my_array[0].length; i ++)
     {
-      System.out.format("%c ", my_array[0][i]);
-    }
-    System.out.format("\n");
-    for ( int j = 0; j < my_array[1].length; j ++)
-    {
-      System.out.format("%c ", my_array[1][j]);
+      for ( int j = 0; j < my_array[i].length; j ++)
+      {
+        System.out.format("%c ", my_array[i][j]);
+      }
+      System.out.format("\n");
     }
     System.out.format("\n");
   }
