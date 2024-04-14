5a07a2241fe42789346b25e1813aab121e7d5cb7
Jackson0077 <158521696+Jackson0077@users.noreply.github.com>
2024-04-13 23:30:46 -0500

    Update Lobby.java

--- src/main/java/uta/cse3310/Lobby.java
+++ src/main/java/uta/cse3310/Lobby.java
@@ -4,13 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lobby {
+
+    public List<String> PlayerNames;
+    public int[] PlayerIds = new int[] {-1, -1, -1, -1};
+    public int[] ReadiedPlayers = new int[] {0, 0, 0, 0};
+    Lobby() {
+        PlayerNames = new ArrayList<>();
+    }
+
+    public void StartGame(){
+        System.out.println("GAME HAS BEEN STARTED!!!!!!!!!!!!!!!!!");
+    }
     
-    ArrayList<Player> PlayersInLobby = new ArrayList<>(4);
-
-    public void AddPlayers(UserEvent U){
-        Player newPlayer = new Player(U.PlayerName, 0, 0, "color", false);
-
-        PlayersInLobby.add(newPlayer);
+    public void AddPlayers(UserEvent U) {
+        PlayerNames.add(U.PlayerName);
+        for(int i = 0; i < 4; i++)
+        {
+            if(PlayerIds[i] == -1)
+            {
+                PlayerIds[i] = U.PlayerIdx;
+                break;
+            }
+        }
         System.out.println("PLAYER SUCCESFULLY ADDED");
+    }
+
+    public void ReadyPlayer(UserEvent U) {
+        for(int j = 0; j < 4; j++)
+        {
+            if(U.PlayerIdx == PlayerIds[j])
+            {
+                ReadiedPlayers[j] = 1;
+                int count = 0;
+                for(int i = 0; i < 4; i++)
+                {
+                    if(ReadiedPlayers[i] == 1)
+                    {
+                        count++;
+                        if(count == PlayerNames.size() && count <= 2)
+                        {
+                            StartGame();
+                        }
+                    }
+                }
+                break;
+            }
+        }
     }
 }
