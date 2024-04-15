3efe284c2a5f41b06229d348a38ec41f10ac9c64
Jackson0077 <158521696+Jackson0077@users.noreply.github.com>
2024-04-15 00:47:05 -0500

    Create LobbyTest.java

---
+++ src/test/java/uta/cse3310/LobbyTest.java
@@ -0,0 +1,38 @@
+package uta;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import uta.cse3310.InitialLobbyTest;
+import uta.cse3310.Lobby;
+import uta.cse3310.UserEvent;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class LobbyTest  extends TestCase{
+
+    public LobbyTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(InitialLobbyTest.class);
+    }
+
+    public void testReadyPlayer(){
+        
+        Lobby lobby1 = new Lobby(1);
+
+        int[] ReadiedPlayers = {0, 1, 0, 0};
+
+        UserEvent userEvent = new UserEvent();
+        userEvent.PlayerIdx = 0;
+        userEvent.Ready = true;
+
+        lobby1.ReadyPlayer(userEvent);
+
+        assertArrayEquals(ReadiedPlayers, lobby1.ReadiedPlayers);
+
+    }
+    
+}
