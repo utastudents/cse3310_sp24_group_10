027828dbeaea046748b7cc3ba4b5bae7a9488856
Jackson0077 <158521696+Jackson0077@users.noreply.github.com>
2024-04-16 12:07:29 -0500

    Update htmlTest.java
    Temporarily commenting out html test

--- src/test/java/uta/cse3310/htmlTest.java
+++ src/test/java/uta/cse3310/htmlTest.java
@@ -1,11 +1,11 @@
 package uta.cse3310;
 
-import junit.framework.TestCase;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import static org.junit.Assert.assertArrayEquals;
+// import junit.framework.TestCase;
+// import junit.framework.TestCase;
+// import junit.framework.TestSuite;
+// import static org.junit.Assert.assertArrayEquals;
 
-public class htmlTest extends TestCase{
-    //
+// public class htmlTest extends TestCase{
+//     //
     
-}
+// }
